f5acd6172b3960ac51b63aeca8268d5309856992
samivnt <samix1.6@gmail.com>
2018-02-28 10:52:54 +0100

    Debut du check first et last name

--- routes/users.js
+++ routes/users.js
@@ -27,6 +27,10 @@ const User = require('../models/user');
  router.post('/', function(req, res, next) {
    // Create a new document from the JSON in the request body
    const newUser = new User(req.body);
+   if(User.find().where('firstName').equals('Coucou').exec()){
+     return res.send("On est dans le if");
+   };
+   //User.find().where('firstName', newUser.firstName)
    // Save that document
    newUser.save(function(err, savedUser) {
      if (err) {
