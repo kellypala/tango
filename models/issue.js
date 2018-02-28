fe66b4884b2e91d5cfb7e91969ded01b062d7561
samivnt <samix1.6@gmail.com>
2018-02-28 09:17:08 +0100

    Add Models for user and issue

---
+++ models/issue.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// Define the schema for users
+const issueSchema = new Schema({
+  status: {
+    type: String,
+    required: true,
+    enum: ['new', 'inProgress', 'canceled', 'completed'],
+    default: 'new'
+  },
+  description: {
+    type: String,
+    maxlength: [1000, 'Description must have 1000 characters max']
+  },
+  imageUrl{
+    type: String,
+    maxlength: [500, 'Url must have 500 characters max']
+  },
+  latitude: {
+    type: Number,
+    required: true
+  },
+  longitude: {
+    type: Number,
+    required: true
+  },
+  tags: [{
+    type: String
+  }],
+  user: {
+    
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date
+  }
+});
+// Create the model from the schema and export it
+module.exports = mongoose.model('Issue', issueSchema);
--- models/user.js
+++ models/user.js
@@ -2,7 +2,29 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // Define the schema for users
 const userSchema = new Schema({
-  name: String
+  firstName: {
+    type: String,
+    required: true,
+    minlength: [2, 'Firstname is too short'],
+    maxlength: [20, 'Firstname is too long']
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minlength: [2, 'Firstname is too short'],
+    maxlength: [20, 'Firstname is too long']
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['citizen', 'manager'],
+    default: 'citizen'
+  },
+  firstName: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
 });
 // Create the model from the schema and export it
 module.exports = mongoose.model('User', userSchema);
