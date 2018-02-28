b615406fd285e588f7dff77abf7572aae6c588b3
kellypala <kelly.pala@heig-vd.ch>
2018-02-28 09:59:20 +0100

    Ajout des routes get et post pour user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user');
 
 /* GET users listing. */
 /**
@@ -12,8 +13,28 @@ var router = express.Router();
  * @apiSuccess {String} firstName First name of the user
  * @apiSuccess {String} lastName  Last name of the user
  */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+ router.get('/', function(req, res, next) {
+   User.find().sort('name').exec(function(err, users) {
+     if (err) {
+       return next(err);
+     }
+     res.send(users);
+   });
+ });
+
+
+ /* POST new user */
+ router.post('/', function(req, res, next) {
+   // Create a new document from the JSON in the request body
+   const newUser = new User(req.body);
+   // Save that document
+   newUser.save(function(err, savedUser) {
+     if (err) {
+       return next(err);
+     }
+     // Send the saved document in the response
+     res.send(savedUser);
+   });
+ });
 
 module.exports = router;
